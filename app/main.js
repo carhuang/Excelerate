3a023e9c75d88633ba5509251bd4c8a5ce056b55
Jillian Kong <jillian.kong@alumni.ubc.ca>
2017-09-30 20:34:52 -0700

    add main js function

--- app/main.js
+++ app/main.js
@@ -1,3 +1,8 @@
+$(function () {
+    console.log("on load");
+    simpleChart();
+});
+
 function countIf(columnName, expValue, csv)
 {
   var data = d3.csvParse();
@@ -16,4 +21,7 @@ function simpleChart()
   var csv = '../data/quiz1a.csv';
   bar1 = countIf('Correct', 'TRUE', csv);
   bar2 = countIf('Correct', 'FALSE', csv);
+
+  document.getElementById("ph1").innerHTML = bar1;
+  document.getElementById("ph2").innerHTML = bar2;
 }
