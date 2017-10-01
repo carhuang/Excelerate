951bd8df765e7e9ecc15740d31f7355d12200578
QuinnHou <queiyun.hou@gmail.com>
2017-10-01 02:54:37 -0700

    committing donut again

--- app/main.js
+++ app/main.js
@@ -20,49 +20,133 @@ $(document).ready(function () {
     //         padAngle = 0.015,
     //         cornerRadius = 3;
     //
-    // }
+    // }/* 
+
 
     d3.csv("../data/quiz1a.csv", sumCorrect, function(err, data) {
-if (err) throw err;
-console.log(data);
-console.log(lastPerCorrect);
-console.log(lastPerIncorrect);
-});
-var numCorrect = 0;
-var numIncorrect = 0;
-var numRows = 0;
-var lastPerCorrect = 0;
-var lastPerIncorrect = 0;
-function sumCorrect(d) {
-    var correctVal;
-    if (d.Correct == "1") {
-        correctVal = 1;
-        numRows ++;
-   } else {
-    correctVal = 0;
-    numRows ++;
-   }
-    if (correctVal == 1){
-    numCorrect ++;
-    numIncorrect = numRows - numCorrect;}
-    var perCorrect = numCorrect/numRows;
-    var perIncorrect = numIncorrect/numRows;
-    lastPerCorrect = perCorrect;
-    lastPerIncorrect = perIncorrect;
-     return {
-       perCorrect, perIncorrect
-        //Question: d.QuestionCode, Unit: d.Unit, perCorrect, perIncorrect
-        //numCorrect, numIncorrect,
-        //Unit: d.Unit, Correct: d.Correct, correctVal, numCorrect, numIncorrect
-    }
-      var dataset = [
-      {state: 'Correct', percent: lastPerCorrect},
-      {state: 'Incorrect', percent: lastPerIncorrect}
-    ];
-    var width = 360;
-    var height = 360;
-    var radius = Math.min(width, height) / 2;
+      if (err) throw err;
+      console.log(data);
+      console.log(perCorrect);
+      console.log(perIncorrect);
 
-  };
+      // data for pie chart
+      var data = [
+        {name: "Correct", value: perCorrect},
+        {name: "Incorrect", value: perIncorrect},
+      ];
+
+      //Copied code from somewhere to display two colour donut chart
+      var text = "";
+      
+      var width = 260;
+      var height = 260;
+      var thickness = 40;
+      var duration = 750;
+      
+      var radius = Math.min(width, height) / 2;
+      var color = d3.scaleOrdinal(d3.schemeCategory10);
+      
+      var svg = d3.select("#chart")
+      .append('svg')
+      .attr('class', 'pie')
+      .attr('width', width)
+      .attr('height', height);
+      
+      var g = svg.append('g')
+      .attr('transform', 'translate(' + (width/2) + ',' + (height/2) + ')');
+      
+      var arc = d3.arc()
+      .innerRadius(radius - thickness)
+      .outerRadius(radius);
+      
+      var pie = d3.pie()
+      .value(function(d) { return d.value; })
+      .sort(null);
+      
+      var path = g.selectAll('path')
+      .data(pie(data))
+      .enter()
+      .append("g")
+      .on("mouseover", function(d) {
+            let g = d3.select(this)
+              .style("cursor", "pointer")
+              .style("fill", "black")
+              .append("g")
+              .attr("class", "text-group");
+       
+            g.append("text")
+              .attr("class", "name-text")
+              .text(`${d.data.name}`)
+              .attr('text-anchor', 'middle')
+              .attr('dy', '-1.2em');
+        
+            g.append("text")
+              .attr("class", "value-text")
+              .text(`${d.data.value}`)
+              .attr('text-anchor', 'middle')
+              .attr('dy', '.6em');
+          })
+        .on("mouseout", function(d) {
+            d3.select(this)
+              .style("cursor", "none")  
+              .style("fill", color(this._current))
+              .select(".text-group").remove();
+          })
+        .append('path')
+        .attr('d', arc)
+        .attr('fill', (d,i) => color(i))
+        .on("mouseover", function(d) {
+            d3.select(this)     
+              .style("cursor", "pointer")
+              .style("fill", "black");
+          })
+        .on("mouseout", function(d) {
+            d3.select(this)
+              .style("cursor", "none")  
+              .style("fill", color(this._current));
+          })
+        .each(function(d, i) { this._current = i; });
+      
+      
+      g.append('text')
+        .attr('text-anchor', 'middle')
+        .attr('dy', '.35em')
+        .text(text);
+
+      });
+      //end of copied code to display two colour donut chart
+
+      //local variables to perform on parsed data from quiz.csv files
+      var numCorrect = 0;
+      var numIncorrect = 0;
+      var numRows = 0;
+      var perCorrect = 0;
+      var perIncorrect = 0;
+      function sumCorrect(d) {
+          var correctVal;
+          if (d.Correct == "1") {
+              correctVal = 1;
+              numRows ++;
+        } else {
+          correctVal = 0;
+          numRows ++;
+        }
+          if (correctVal == 1){
+          //acculmulate number total correct/incorrect, while iterating through the rows
+          numCorrect ++;
+          numIncorrect = numRows - numCorrect;}
+
+          //calculate the return values as fractions out of 100 (like percentage)
+          //but I can't find a way for the donut chart to interpret percentages meaninfully and display it
+          //so it's in decimal for now
+          perCorrect = numCorrect/numRows;
+          perCorrect = perCorrect.toFixed(2);
+          perIncorrect = numIncorrect/numRows;
+          perIncorrect = perIncorrect.toFixed(2);
+  
+          return {
+            perCorrect, perIncorrect
+          }
+      };
 });
 
