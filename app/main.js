a833f084342a06aba2e06265497232d94ab7bef4
QuinnHou <queiyun.hou@gmail.com>
2017-10-01 11:25:35 -0700

    in the middle of adding course summary

--- app/main.js
+++ app/main.js
@@ -60,6 +60,16 @@ $(document).ready(function () {
             renderDonutChart(quizUnitDonutData, '#unitChart1');
 
         });
+
+         
+        d3.csv("../data/courseSumm.csv", function (err, data) {
+            if (err) throw err;
+            console.log(data);
+
+            renderDonutChart(data, '#courseSumm');
+    
+
+        });
     
     
         function getQuizDonutData(data) {
@@ -168,6 +178,87 @@ $(document).ready(function () {
     
     
         function renderDonutChart(data, id) {
+            var text = "";
+
+            var width = 260;
+            var height = 260;
+            var thickness = 40;
+            var duration = 750;
+
+            var radius = Math.min(width, height) / 2;
+            var color = d3.scaleOrdinal(d3.schemeCategory10);
+    
+            var svg = d3.select(id)
+                .append('svg')
+                .attr('class', 'pie')
+                .attr('width', width)
+                .attr('height', height);
+
+            var g = svg.append('g')
+                .attr('transform', 'translate(' + (width / 2) + ',' + (height / 2) + ')');
+
+            var arc = d3.arc()
+                .innerRadius(radius - thickness)
+                .outerRadius(radius);
+
+            var pie = d3.pie()
+                .value(function (d) { return d.percent; })
+                .sort(null);
+
+            var path = g.selectAll('path')
+                .data(pie(data))
+                .enter()
+                .append("g")
+                .on("mouseover", function (d) {
+                    let g = d3.select(this)
+                        .style("cursor", "pointer")
+                        .style("fill", "black")
+                        .append("g")
+                        .attr("class", "text-group");
+
+                    g.append("text")
+                        .attr("class", "state-text")
+                        .text(`${d.data.state}`)
+                        .attr('text-anchor', 'middle')
+                        .attr('dy', '-1.2em');
+
+                    g.append("text")
+                        .attr("class", "percent-text")
+                        .text(`${d.data.percent}`)
+                        .attr('text-anchor', 'middle')
+                        .attr('dy', '.6em');
+                })
+                .on("mouseout", function (d) {
+                    d3.select(this)
+                        .style("cursor", "none")
+                        .style("fill", color(this._current))
+                        .select(".text-group").remove();
+                })
+                .append('path')
+                .attr('d', arc)
+                .attr('fill', (d, i) => color(i))
+                .on("mouseover", function (d) {
+                    d3.select(this)
+                        .style("cursor", "pointer")
+                        .style("fill", "black");
+                })
+                .on("mouseout", function (d) {
+                    d3.select(this)
+                        .style("cursor", "none")
+                        .style("fill", color(this._current));
+                })
+                .each(function (d, i) { this._current = i; });
+
+
+            g.append('text')
+                .attr('text-anchor', 'middle')
+                .attr('dy', '.35em')
+                .text(text);
+        }
+
+
+
+        function renderCourseSum(data, id) {
             var text = "";
 
             var width = 260;
