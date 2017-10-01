34c0b11346350fdfe11f27e685916323a87c2f77
QuinnHou <queiyun.hou@gmail.com>
2017-10-01 03:26:24 -0700

    deleted redudant comments

--- app/main.js
+++ app/main.js
@@ -1,27 +1,6 @@
 $(document).ready(function () {
     console.log("on load");
 
-//     var quiz1a = quizChart();
-//
-//     d3.csv("../data/quiz1a.csv", sumCorrect, function(err, data) {
-//     if (err) throw err;
-//     console.log(data);
-//     d3.select('#quiz1a')
-//             .datum(data)
-//             .call(quiz1a);
-// });
-
-
-    // function quizChart() {
-    //     var width = 960,
-    //         height = 500,
-    //         margin = {top:10, right: 10, bottom:10, left:10},
-    //         colour = d3.scaleOrdinal(d3.schemeCategory20c),
-    //         padAngle = 0.015,
-    //         cornerRadius = 3;
-    //
-    // }/* 
-
 
     d3.csv("../data/quiz1a.csv", sumCorrect, function(err, data) {
       if (err) throw err;
